6e1da25eace33f3710d92f08c4728e7cf4bc35e8
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-02-15 19:51:27 +0200

    spacing

--- Program/ComputerLogic.cs
+++ Program/ComputerLogic.cs
@@ -8,5 +8,42 @@ namespace A24_Ex02
 {
     internal class ComputerLogic
     {
+        internal enum eAiType
+        {
+            RandomAi,
+            AiLevel1,
+            AiLevel2,
+        }
+
+        eAiType m_AiType = eAiType.RandomAi;
+
+        internal eAiType m_AiType
+        {
+            get { }
+        }
+
+        internal byte GetTokenPlacemeant(Connect4BoardLogic.Connect4Board i_Board)
+        {
+            byte o_ChosenColumn;
+            switch(m_AiType)
+            {
+                case eAiType.RandomAi:
+                    o_ChosenColumn = GetRandomColumn(ref i_Board);
+                    break;
+
+                default:
+                    break;
+                        
+            }
+
+            return o_ChosenColumn;
+        }
+
+        private byte GetRandomColumn(ref Connect4BoardLogic.Connect4Board i_Board)
+        {
+            byte o_ChosenColumn;
+
+            return o_ChosenColumn;
+        }
     }
 }
